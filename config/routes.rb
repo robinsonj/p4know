692231e5cb4fe413545a2fc4fb0ed79f464eefe6
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-23 09:57:21 -0700

    Fix thing name autocompletion path
    Fix the route name to match the controller action provided by the rails
    autocompletion gem and add an alias to make it more readable.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ P4know::Application.routes.draw do
   root to: 'home#index'
 
   resources :things, only: [] do
-    get :autocomplete_name, on: :collection
+    get :autocomplete_thing_name, on: :collection, as: :autocomplete_names
   end
 end
