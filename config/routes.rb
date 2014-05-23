49fd6031b11fbf21d239731c3a9d4ae85610eb99
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-23 12:26:04 -0700

    Add a route to the session creation action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 P4know::Application.routes.draw do
   root to: 'home#index'
 
+  post 'sessions/create'
+
   resources :things, only: [] do
     get :autocomplete_thing_name, on: :collection, as: :autocomplete_names
   end
