7c55dee53afdcb9a377329d4614273533a5a7dbc
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-23 09:58:55 -0700

    Add routes to the future controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 P4know::Application.routes.draw do
   root to: 'home#index'
 
+  post 'set_knowledge', to: 'things#set_knowledge'
+  get  'get_knowledge', to: 'things#get_knowledge'
+
   resources :things, only: [] do
     get :autocomplete_name, on: :collection
   end
