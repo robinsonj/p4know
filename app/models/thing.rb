91c6bd2f86718d7e1eaa9bb6a6d86039f7639c65
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-23 10:59:22 -0700

    Add Thing class method
    Create a method for retrieving a list of all Thing records, new or pre-
    existing, that have names matching 'thing_names.'

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -3,4 +3,13 @@ class Thing < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
+  def self.from_string(thing_names)
+    things = []
+
+    thing_names.strip.gsub(/ +/, '').split(/,/).each do |thing_name|
+      things.push(find_or_create_by(name: thing_name)) unless thing_name.blank?
+    end
+
+    things
+  end
 end
