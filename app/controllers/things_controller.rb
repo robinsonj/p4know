624d9df6d2adade94901081df354871978833503
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-23 10:03:52 -0700

    Stub Things controller actions + param handling

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,4 +1,24 @@
 class ThingsController < ApplicationController
 
   autocomplete :thing, :name, full: false
+
+  # POST /set_knowledge
+  def set_knowledge
+
+
+    render :refresh
+  end
+
+  # GET /get_knowledge
+  def get_knowledge
+
+
+    render :refresh
+  end
+
+  private
+
+  def thing_params
+    params.require()
+  end
 end
