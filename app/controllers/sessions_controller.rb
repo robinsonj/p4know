0bcb385b0342b536e368cdd16be0b9cfeb548930
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-23 17:15:42 -0700

    Fill out session creation action
    Find or create user records based on the name passed through to the
    controller. Once the record has been instantiated, authenticate the
    user through perforce.
    
    TODO:
      * Change order of user record creation and authentication. Currently,
        a record us created then the name and password provided are passed
        through to the perforce server. Switch the order so that records
        with invalid perforce usernames are not created since they will
        never be used.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,30 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find_or_create_by(name: user_name_param)
+
+    if @user.authenticate(user_password_param)
+      session[:user] = @user
+
+      flash[:info] = 'Logged in.'
+      redirect_to :back
+    else
+      flash[:error] = 'Failed login attempt.'
+      redirect_to :back
+    end
   end
 
   def destroy
     session[:user] = nil
     redirect_to :back, notice: 'Logged out.'
+  end
+
+  private
+
+  def user_name_param
+    params.require(:username)
+  end
+
+  def user_password_param
+    params.require(:password)
   end
 end
