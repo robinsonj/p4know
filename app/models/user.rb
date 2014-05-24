122565ae0b4ce9975cb2f4f9029412f2189bf431
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-23 17:14:52 -0700

    Add authentication methods to the User model
    Authenticate through a perforce server, passing in the username and
    password users provide through the sessions controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :things
 
+  def authenticate(password)
+    p4_login(name, password)
+  end
+
+  private
+
+  def p4_login(username, password)
+    result = `echo #{password} | p4 -p #{P4PORT} -u #{username} login`
+
+    if (/User .* logged in./ =~ result)
+      true
+    else
+      false
+    end
+  end
+
 end
