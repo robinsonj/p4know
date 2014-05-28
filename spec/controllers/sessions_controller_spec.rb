81b36d7aa76112fe71c9a4a63651e20fb9ac1ed4
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-28 12:09:25 -0700

    Fill out controller specs
    Expect all actions to redirect except for the main page.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,10 +2,23 @@ require 'spec_helper'
 
 describe SessionsController do
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
+  before(:each) do
+    @request.env['HTTP_REFERER'] = 'http://localhost:3000/'
+  end
+
+  describe "POST 'create'" do
+    it 'returns http redirect' do
+      post :create, { username: 'testuser', password: 'test' }
+
+      expect(response).to be_redirect
+    end
+  end
+
+  describe "POST 'destroy'" do
+    it 'returns http redirect' do
+      post :destroy
+
+      expect(response).to be_redirect
     end
   end
 
--- spec/controllers/things_controller_spec.rb
+++ spec/controllers/things_controller_spec.rb
@@ -2,4 +2,24 @@ require 'spec_helper'
 
 describe ThingsController do
 
+  before(:each) do
+    @request.env['HTTP_REFERER'] = 'http://localhost:3000/'
+  end
+
+  describe "GET 'get_knowledge'" do
+    it 'returns http redirect' do
+      get :get_knowledge, { thing_names: 'test, test2' }
+
+      expect(response).to be_redirect
+    end
+  end
+
+  describe "POST 'set_knowledge'" do
+    it 'returns http redirect' do
+      post :set_knowledge, { thing_names: 'test, test2' }
+
+      expect(response).to be_redirect
+    end
+  end
+
 end
